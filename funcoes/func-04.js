6e406cbac05e90603a00da8173d75955e79a41fb
Felipipi <felipi.costa@escola.pr.gov.br>
2026-04-08 12:36:59 +0000

    Ajustes

--- funcoes/func-04.js
+++ funcoes/func-04.js
@@ -8,8 +8,8 @@ function sobraDoSalario(meuSalario){
     const aluguel = 1500;
     const contaDeLuz = 150;
     const resto = meuSalario - (aluguel + contaDeLuz);
-    return `O que sobrou do meu salário foi: ${resto}`;
+    return `O que sobrou do meu salário foi R$ ${resto.toFixed(2).replace('.', ',')}`;
 }
 const minhasSobras = sobraDoSalario(1700.00);
-console.log(sobraDoSalario(25000.00.replace('.',',')));
+console.log(sobraDoSalario(25000.00));
 console.log(minhasSobras);
