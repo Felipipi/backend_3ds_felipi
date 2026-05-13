b7447f0835692577165b489f0bde779e82593dbe
Felipipi <felipi.costa@escola.pr.gov.br>
2026-05-13 11:40:16 +0000

    Função ESM Gatos banho e tosa

---
+++ 2ºTRIMESTRE/modulos/mod02-ESM/main-mod-gatos.js
@@ -0,0 +1,7 @@
+
+import {banhoGato, tosaGato, alimentoGato} from './mod-gatos.js';
+
+let racaViraLata = "Vira-Lata"
+console.log(banhoGato('Siamês', 'Completo'));
+console.log(tosaGato('Persa', 'Baixa'));
+console.log(alimentoGato('Angorá', 'Premium'));
--- 2ºTRIMESTRE/modulos/mod02-ESM/mod-gatos.js
+++ 2ºTRIMESTRE/modulos/mod02-ESM/mod-gatos.js
@@ -1,10 +1,13 @@
-let racaViraLata = "Vira-Lata"
-export function banhoGato(racaGato, tipoBanho){
 
-}
+export function banhoGato(racaGato, tipoBanho){
+    return `Gato ${racaGato} fez um banho: ${tipoBanho}.`
+};
+
+
 export function tosaGato(racaGato, tipoTosa){
-    
-}
-export function alimentoGato(racaoGato, tipoRacao){
-    
+    return `Gato ${racaGato} fez uma tosa: ${tipoTosa}.`
+};
+
+export function alimentoGato(racaGato, tipoRacao){
+    return `Gato ${racaGato} foi alimentado com: ${tipoRacao}.`  
 }
