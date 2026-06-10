2a49e084d8cdc69db8a09fbba5d3708ef970f74b
Felipipi <felipi.costa@escola.pr.gov.br>
2026-06-10 11:42:50 +0000

    Teste chalk no js

--- 2ºTRIMESTRE/modulos/mod03-fs/fs-chalk.js
+++ 2ºTRIMESTRE/modulos/mod03-fs/fs-chalk.js
@@ -3,20 +3,22 @@
 // para deduzir subdiretórios e S.O
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk').default
 
 //salvar o caminho como uma variável
 // const caminho = path.join(__dirname, "README.md");//markdown, text
 try{
-const caminho = path.resolve(__dirname, "READE.md");//Abrir, ler, deletar, editar
+const caminho = path.resolve(__dirname, "README.md");//Abrir, ler, deletar, editar
 
 const arquivo = fs.readFileSync(caminho, 'utf-8');
+console.log(chalk.green('Arquivo carregado com sucesso!'))
 console.log(arquivo);
 
 } catch(error){
     // console.log(error.errno);
-       console.log('Falha ao carregar o arquivo. Tente novamente mais tarde')
+       console.log(chalk.yellow('Falha ao carregar o arquivo. Tente novamente mais tarde'))
 
-   console.log('Erro interno de leitura de arquivo \n', error.message)
+   console.log(chalk.red('Erro interno de leitura de arquivo \n'), chalk.white(error.message))
 }
 
 
