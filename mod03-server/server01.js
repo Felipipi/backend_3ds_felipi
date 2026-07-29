eb0cc0768ec4f578b6abce4a261fb708dedf4722
Felipipi <felipi.costa@escola.pr.gov.br>
2026-07-29 11:37:51 +0000

    Index da nossa página

--- mod03-server/server01.js
+++ mod03-server/server01.js
@@ -3,10 +3,18 @@ const http = require('node:http');
 const porta = 8002
 
 const server = http.createServer((req, res) => {
-    res.statusCode = '201'
-   //res.setHeader('Content-Type', 'text/plain')
-    res.setHeader('Content-Type', 'text/html', 'charset=utf-8')
-    res.end('<h3>HTML Rocks!</h3>')
+    const novaUrl = new URL(req.url, `http://${req.headers.host}`) //http://localhost
+    const caminhoUrl = novaUrl.pathname
+    if(caminhoUrl === '/'){
+        res.statusCode = '201'
+       //res.setHeader('Content-Type', 'text/plain')
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end('<h3>Bem-vindo à nossa página!</h3>')
+    } else {
+        res.statusCode = '404'
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end('<h3>404 Página Não Encontrada</h3>')
+    }
 })
 
 server.listen(porta, () => {
