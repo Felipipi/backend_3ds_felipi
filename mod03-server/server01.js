866f0acb82fdef8400cabdeefe40d9838f2d2eaf
Felipipi <felipi.costa@escola.pr.gov.br>
2026-08-25 11:44:01 +0000

    Ajuste de abas e estilização css com IA

--- mod03-server/server01.js
+++ mod03-server/server01.js
@@ -7,26 +7,39 @@ const porta = 8081
 const inicio = path.join(__dirname, 'pages/inicio.html')
 const sobre = path.join(__dirname, 'pages/sobre.html')
 const erro = path.join(__dirname, 'pages/erro.html')
+const css = path.join(__dirname, 'pages/public/estilo.css')
+const image = path.join(__dirname, 'pages/public/images/carro.png')
 
 const server = http.createServer((req, res) => {
     const novaUrl = new URL(req.url, `http://${req.headers.host}`) //http://localhost
     const caminhoUrl = novaUrl.pathname
 
-    if(caminhoUrl === '/'){
-        res.statusCode = '201'
-       //res.setHeader('Content-Type', 'text/plain')
-        res.setHeader('Content-type', 'text/html; charset=utf-8')
-        return res.end(fs.readFileSync(inicio, 'utf-8')) //chamar o html
+    if(caminhoUrl === '/public/estilo.css'){
+        res.statusCode = 200
+        res.setHeader('Content-type', 'text/css')
+        return res.end(fs.readFileSync(css))
     }
-      if(caminhoUrl === '/sobre'){
-        res.statusCode = '201'
-       //res.setHeader('Content-Type', 'text/plain')
+
+    if(caminhoUrl === '/images/carro.png'){
+        res.statusCode = 200
+        res.setHeader('Content-type', 'image/png')
+        return res.end(fs.readFileSync(image))
+    }
+
+    if(caminhoUrl === '/' || caminhoUrl === '/inicio'){
+        res.statusCode = 200
         res.setHeader('Content-type', 'text/html; charset=utf-8')
-        return res.end(fs.readFileSync(sobre, 'utf-8')) //chamar o html
+        return res.end(fs.readFileSync(inicio, 'utf-8'))
+    }
+
+      if(caminhoUrl === '/sobre'){
+        res.statusCode = 200
+        res.setHeader('Content-type', 'text/html; charset=utf-8')
+        return res.end(fs.readFileSync(sobre, 'utf-8'))
     } 
 
     else {
-        res.statusCode = '404'
+        res.statusCode = 404
         res.setHeader('Content-Type', 'text/html; charset=utf-8')
         res.end(fs.readFileSync(erro, 'utf-8'))
     }
